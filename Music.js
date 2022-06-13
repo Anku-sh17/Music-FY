fa56f29a5e3c2aa0b9074c4f2c7404931290f6be
Anku-sh17 <97309508+Anku-sh17@users.noreply.github.com>
2022-06-13 16:01:05 +0530

    updated

--- Music.js
+++ Music.js
@@ -10,31 +10,14 @@ const TopArtistsection = document.querySelector(".TopArtist");
 const Trendingsection = document.querySelector(".Trending-display");
 const mainsection = document.querySelector(".main");
 
-// each artists section
-// const harrysection = document.querySelector(".harrywrap");
-// const billiesection = document.querySelector(".billiewrap");
-// const edsheeransection = document.querySelector(".edsheeranwrap");
-// const arianasection = document.querySelector(".arianawrap");
-
 // Music Card buttons
 const Play = document.querySelector(".playpause-track");
-const volume = document.querySelector(".volume-track");
-const like = document.querySelector(".like-track");
+const imageofart = document.querySelector(".img");
+// const volume = document.querySelector(".volume-track");
+// const like = document.querySelector(".like-track");
 
 // Songs Cards Buttons
-const harrystyles = document.querySelector(".imaf_HarryStyles");
-const BillieEilish = document.querySelector(".imaf_BillieEilish");
-const edsheeran = document.querySelector(".imaf_EdSheeran");
-const arianagrande = document.querySelector(".imaf_ArianaGrande");
-const zyanmalik = document.querySelector(".imaf_ZyanMalik");
-const dualipa = document.querySelector(".imaf_DuaLipa");
-const taylorswift = document.querySelector(".imaf_TaylorSwift");
-const bts = document.querySelector(".imaf_BTS");
-const justinbieber = document.querySelector(".imaf_JustinBieber");
-const shawnmendes = document.querySelector(".imaf_ShawnMendes");
-const selenagomez = document.querySelector(".imaf_SelenaGomes");
-const blackpink = document.querySelector(".imaf_Blackpink");
-const allimages = document.querySelector(".imafx");
+const artistnames = document.querySelectorAll(".TopArtist");
 
 // Artist Name Buttons
 const playlistharrywrap = document.querySelector(".harrywrap");
@@ -54,18 +37,40 @@ const playlistblackpinkwrap = document.querySelector(".blackpinkwrap");
 const contents = document.querySelector(".contents");
 const loader = document.querySelector(".loader");
 
-// favorites button
-const playfav = document.querySelector(".playfavtrack");
+//login
+const logbtn = document.querySelector(".LogIn");
+let inputloginname = document.getElementById("username");
+let inputpin = document.getElementById("password");
 
-//loader
-// const start = function () {
-//   contents.classList.remove("hidden");
-//   loader.classList.add("hidden");
-// };
+// All Songs selection
+const allsongs = document.querySelectorAll(".wrapper");
 
-// window.onload = (e) => {
-//   setTimeout(start, 3000);
-// };
+const accounts = [
+  {
+    username: "Ankush",
+    pin: 0000,
+  },
+  {
+    username: "Ayush",
+    pin: 1111,
+  },
+  {
+    username: "Aditya",
+    pin: 2222,
+  },
+  {
+    username: "Suyash",
+    pin: 3333,
+  },
+  {
+    username: "Arav",
+    pin: 4444,
+  },
+  {
+    username: "Nitin",
+    pin: 5555,
+  },
+];
 
 // Navigation Bar switching
 
@@ -88,7 +93,7 @@ const sections = [
   playlistzyanwrap,
 ];
 
-const work = function (sec) {
+const displayfunction = function (sec) {
   for (let i = 0; i < sections.length; i++) {
     if (!sections[i].classList.contains("hidden") && sections[i] != sec) {
       sections[i].classList.add("hidden");
@@ -97,242 +102,98 @@ const work = function (sec) {
 };
 
 home.addEventListener("click", function () {
-  work(mainsection);
+  displayfunction(mainsection);
   mainsection.classList.remove("hidden");
 });
 
 About.addEventListener("click", function () {
-  work(Aboutsection);
+  displayfunction(Aboutsection);
   Aboutsection.classList.remove("hidden");
 });
 
 topartist.addEventListener("click", function () {
-  work(TopArtistsection);
+  displayfunction(TopArtistsection);
   TopArtistsection.classList.remove("hidden");
 });
 
 trending.addEventListener("click", function () {
-  work(Trendingsection);
+  displayfunction(Trendingsection);
   Trendingsection.classList.remove("hidden");
 });
-
-// Music Playing
-
-/*
-let songsofartist = [
-  {
-    artistname: "Ed-sheeran",
-    Song: "Shape of You",
-    image:
-      "https://st2.depositphotos.com/6694302/9685/i/600/depositphotos_96855090-stock-photo-ed-sheeran-concert.jpg",
-    path: "ShapeOfYou-Edsheeran.mp3",
-    id: 1,
-  },
-  {
-    artistname: "Zyan",
-    Song: "Dusk Till Dawn",
-    image:
-      "https://aniportalimages.s3.amazonaws.com/media/details/zayynuuumsdfs2022011512393420220115124425.jpg",
-    path: "DuskTillDawn-Zyan.mp3",
-    id: 2,
-  },
-  {
-    artistname: "Billie Eilish",
-    Song: "Lovely",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/thumb/f/fa/Billie_Eilish_2019_by_Glenn_Francis_%28cropped%29_2.jpg/800px-Billie_Eilish_2019_by_Glenn_Francis_%28cropped%29_2.jpg",
-    path: "Lovely-BillieEilish.mp3",
-    id: 3,
-  },
-  {
-    artistname: "Ariana Grande",
-    Song: "Positions",
-    image: "https://i.scdn.co/image/ab6761610000e5ebcdce7620dc940db079bf4952",
-    path: "Positions-Ariana.mp3",
-    id: 4,
-  },
-  {
-    artistname: "Harry Styles",
-    Song: "WaterMelon Sugar",
-    image:
-      "https://media.npr.org/assets/img/2020/02/27/wide-use_hpromophoto_helenepambrun-72fdb64792139d94a06f18686d0bb3131a238a70-s1100-c50.jpg",
-    path: "WaterMelon Sugar-Harry Styles.mp3",
-    id: 5,
-  },
-  {
-    artistname: "Dua Lipa",
-    Song: "Levitating",
-    image:
-      "https://upload.wikimedia.org/wikipedia/commons/9/95/Dua_Lipa_with_Warner_Music.jpg",
-    path: "Levitating - Dua Lipa.mp3",
-    id: 6,
-  },
-  {
-    artistname: "Taylor Swaift",
-    Song: "Love Story",
-    image: "https://i1.sndcdn.com/avatars-000500544273-6kcyh0-t500x500.jpg",
-    path: "Love Story - Taylor Swift.mp3",
-    id: 7,
-  },
-  {
-    artistname: "BTS",
-    Song: "Dynamite",
-    image:
-      "https://pyxis.nymag.com/v1/imgs/c10/524/1853b57c011cf5fd8ebc8f4b149345d5d3-BTS-JOIN-LOUIS-VUITTON-AS-HOUSE-AMBASSAD.rsquare.w1200.jpg",
-    path: "BTS-DYNAMITE.mp3",
-    id: 8,
-  },
-  {
-    artistname: "Justin Bieber",
-    Song: "Stay",
-    image:
-      "https://media.gq.com/photos/56bcb218cdf2db6945d2ef93/4:3/w_2000,h_1500,c_limit/bieber-coverstory-square.jpg",
-    path: "Stay - Justin Bieber.mp3",
-    id: 9,
-  },
-  {
-    artistname: "Shwan Mendes",
-    Song: "Nothin Holding me Back",
-    image:
-      "https://yt3.ggpht.com/QTpsceScLWmzgZXO6lPJo-s0CzCaGoat2S8PZzfdKtvqJTUh7jCLtYvCPH1RbUzVCOz2t_RIqIA=s900-c-k-c0x00ffffff-no-rj",
-    path: "Nothing Holding me back- Shawn Mendes.mp3",
-    id: 10,
-  },
-  {
-    artistname: "Selena Gomez",
-    Song: "We Don't Talk Anymore",
-    image:
-      "https://m.media-amazon.com/images/M/MV5BMjAwNTE2MDMyMl5BMl5BanBnXkFtZTgwMjAyODM3MTI@._V1_UY1200_CR134,0,630,1200_AL_.jpg",
-    path: "Charlie Puth - We Don't Talk Anymore.mp3",
-    id: 11,
-  },
-  {
-    artistname: "BlackPink",
-    Song: "Ice-Cream",
-    image:
-      "https://img.i-scmp.com/cdn-cgi/image/fit=contain,width=1098,format=auto/sites/default/files/styles/1200x800/public/d8/images/methode/2020/10/16/41e9b47c-07af-11eb-afc8-92e0da0ef1c3_image_hires_155550.jpg?itok=ihKswLDq&v=1602834958",
-    path: "Icrecream-Blackpink.mp3",
-    id: 12,
-  },
-];
-*/
 
 let isplaying = false;
 let currentid = 0;
 let currenttrack = document.createElement("audio");
 let currenttrend = 1;
 
-const songcards = [
-  edsheeran,
-  zyanmalik,
-  BillieEilish,
-  arianagrande,
-  harrystyles,
-  dualipa,
-  taylorswift,
-  bts,
-  justinbieber,
-  shawnmendes,
-  selenagomez,
-  blackpink,
-];
-
 const eachplaylist = [
   playlistsheeranwrap,
-  playlistzyanwrap,
   playlistartistbilliewrap,
-  playlistarianawrap,
   playlistharrywrap,
-  playlistduawrap,
+  playlistarianawrap,
+  playlistzyanwrap,
+  playlistblackpinkwrap,
   playlisttaylorwrap,
   playlistbtswrap,
   playlistjustinwrap,
   playlistshawnwrap,
   playlistselenawrap,
-  playlistblackpinkwrap,
+  playlistduawrap,
 ];
 
-// const songartist = [
-//   nameofartistedsheeran,
-//   nameofartistzyanmalik,
-//   nameofartistBillieEilish,
-//   nameofartistarianagrande,
-//   nameofartistharrystyles,
-//   nameofartistdualipa,
-//   nameofartisttaylorswift,
-//   nameofartistbts,
-//   nameofartistjustinbieber,
-//   nameofartistshawnmendes,
-//   nameofartistselenagomez,
-//   nameofartistblackpink,
-// ];
-
-like.addEventListener("click", function () {
-  let songname;
-  let artname;
-  songsofartist.forEach((m) => {
-    if (m.id == currentid) {
-      songname = m.Song;
-      artname = m.artistname;
-    }
-  });
-  const html = `<div class="trending${currenttrend}">
-  <div style="font-size: 2rem">${songname}</div>
-  <div style="font-size: 1.3rem">${artname}</div>
-  <div class ="playfavtrack">
-  <i class="fas fa-play" style="font-size: 1.5rem"></i>
-  </div>
-  <div class ="delfavtrack">
-  <i class="fas fa-trash" style="font-size: 1.5rem"></i>
-  </div>
-</div>`;
-  if (currenttrend == 2) {
-    currenttrend = 1;
-  } else {
-    currenttrend = 2;
+// Login activities
+let curraccoun;
+logbtn.addEventListener("click", function (e) {
+  e.preventDefault();
+  curraccoun = accounts.find((acc) => acc.username === inputloginname.value);
+  if (curraccoun == undefined) {
+    alert("Invalid username");
+  } else if (curraccoun?.pin === Number(inputpin.value)) {
+    loader.classList.add("hidden");
+    contents.classList.remove("hidden");
+  } else if (curraccoun?.pin != Number(inputpin.value)) {
+    alert("Invalid Pin or UserName");
   }
-  Trendingsection.insertAdjacentHTML("afterbegin", html);
 });
 
 Play.addEventListener("click", function () {
   if (!isplaying) {
     currenttrack.play();
     isplaying = true;
-    const html = `<i class="fas fa-pause" style="font-size: 3rem"></i>`;
+    const html = `<i class="bx bx-pause" style="font-size: 5rem"></i>`;
     Play.innerHTML = "";
     Play.insertAdjacentHTML("afterbegin", html);
   } else {
     currenttrack.pause();
     isplaying = false;
-    const html = `<i class="fas fa-play" style="font-size: 3rem"></i>`;
+    const html = `<i class="bx bx-play" style="font-size: 5rem"></i>`;
     Play.innerHTML = "";
     Play.insertAdjacentHTML("afterbegin", html);
   }
 });
 
-const playsong = function () {
-  for (let j = 0; j < songsofartist.length; j++) {
-    if (songsofartist[j].id === currentid) {
-      document.querySelector(".track-artist").textContent =
-        songsofartist[j].artistname;
-      document.querySelector(".track-name").textContent = songsofartist[j].Song;
-      work(mainsection);
-      mainsection.classList.remove("hidden");
-      currenttrack.src = songsofartist[j].path;
-      currenttrack.play();
-      const html = `<i class="fas fa-pause" style="font-size: 3rem"></i>`;
-      Play.innerHTML = "";
-      Play.insertAdjacentHTML("afterbegin", html);
-      isplaying = true;
-    }
-  }
-};
-
-for (let i = 0; i < songcards.length; i++) {
-  songcards[i].addEventListener("click", function () {
-    currentid = i + 1;
-    work(eachplaylist[i]);
-    eachplaylist[i].classList.remove("hidden");
-    // playsong();
+artistnames.forEach((artist) => {
+  artist.addEventListener("click", function (e) {
+    console.log(e.path[0].id);
+    displayfunction(eachplaylist[e.path[0].id]);
+    eachplaylist[e.path[0].id].classList.remove("hidden");
   });
-}
+});
+
+allsongs.forEach((box) => {
+  box.addEventListener("click", function (event) {
+    document.querySelector(".track-name").textContent = event.path[1].className;
+    document.querySelector(".track-artist").textContent = event.path[1].id;
+    const html1 = `<img src=${event.path[0].currentSrc} alt="img" />`;
+    document.querySelector(".img").innerHTML = "";
+    document.querySelector(".img").insertAdjacentHTML("afterbegin", html1);
+    currenttrack.src = `Database/${event.path[1].className}.mp3`;
+    currenttrack.play();
+    displayfunction(mainsection);
+    mainsection.classList.remove("hidden");
+    const html = `<i class="bx bx-pause" style="font-size: 5rem"></i>`;
+    Play.innerHTML = "";
+    Play.insertAdjacentHTML("afterbegin", html);
+    isplaying = true;
+  });
+});
